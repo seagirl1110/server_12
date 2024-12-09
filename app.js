6b8f6ec534a56097166a9b4b7e55ed7f0538f872
Marina Elizarova <seagirl1110@gmail.com>
2024-12-09 21:31:01 +0300

    feat: add get-query /products and /products/:id

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'dotenv/config';
+import { ObjectId } from 'mongodb';
 import { connectToDatabase, getDB } from './db/index.js';
 
 const app = express();
@@ -29,6 +30,46 @@ connectToDatabase()
         res
           .status(500)
           .json({ message: `Failed to create product: ${error.message}` });
+      }
+    });
+
+    app.get('/products', async (req, res) => {
+      try {
+        const db = getDB();
+
+        const products = await db.collection('products').find().toArray();
+
+        res.status(200).json(products);
+      } catch (error) {
+        res
+          .status(500)
+          .json({ message: `Failed to fetch products: ${error.message}` });
+      }
+    });
+
+    app.get('/products/:id', async (req, res) => {
+      try {
+        const db = getDB();
+
+        const id = req.params.id;
+
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).json({ message: 'Invalid product id' });
+        }
+
+        const product = await db
+          .collection('products')
+          .findOne({ _id: new ObjectId(id) });
+
+        if (!product) {
+          return res.status(404).json({ message: 'Product not found' });
+        }
+
+        res.status(200).json(product);
+      } catch (error) {
+        res
+          .status(500)
+          .json({ message: `Failed to fetch product: ${error.message}` });
       }
     });
 
