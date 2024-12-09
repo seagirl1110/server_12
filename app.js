cfe247c0ac88a5716df648ffad25e3a70fe922ff
Marina Elizarova <seagirl1110@gmail.com>
2024-12-09 21:16:38 +0300

    feat: add post-query /products

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 import express from 'express';
 import 'dotenv/config';
-import { connectToDatabase } from './db/index.js';
+import { connectToDatabase, getDB } from './db/index.js';
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.PORT;
 
 connectToDatabase()
   .then(() => {
+    app.post('/products', async (req, res) => {
+      try {
+        const db = getDB();
+
+        const { name, price, description } = req.body;
+        if (!name || !price || !description) {
+          return res
+            .status(400)
+            .json('message: Name, price and description are required');
+        }
+
+        const result = await db
+          .collection('products')
+          .insertOne({ name, price, description });
+
+        res.status(201).json(result);
+      } catch (error) {
+        res
+          .status(500)
+          .json({ message: `Failed to create product: ${error.message}` });
+      }
+    });
+
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
     });
